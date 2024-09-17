60691a7332968f4a6f47d2df9bf27a99b2209f04
foziljonov7 <abdulvosidfoziljonov55@gmail.com>
2024-09-17 15:21:37 +0500

    Rename UserServices

--- Backend/Chayxana.BLL/obj/Debug/net8.0/Chayxana.BLL.AssemblyInfo.cs
+++ Backend/Chayxana.BLL/obj/Debug/net8.0/Chayxana.BLL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Chayxana.BLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9fdbdf9b4f1db112c39525eec2a0360e6b163e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+781a3451ed3eaf169a40cdccab2776dcd7df5a5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Chayxana.BLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Chayxana.BLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
