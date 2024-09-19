01d0b7b4349686c6fffcfd4404931adce7f54c4e
foziljonov7 <abdulvosidfoziljonov55@gmail.com>
2024-09-19 22:23:06 +0500

    ...

--- Backend/Chayxana.DAL/obj/Debug/net8.0/Chayxana.DAL.AssemblyInfo.cs
+++ Backend/Chayxana.DAL/obj/Debug/net8.0/Chayxana.DAL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Chayxana.DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9376be0f41ba2e7a0c08ad446b4e26028969988")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea7bc5c4d081a12fa40e9bbf888faf9643b1aee1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Chayxana.DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Chayxana.DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
