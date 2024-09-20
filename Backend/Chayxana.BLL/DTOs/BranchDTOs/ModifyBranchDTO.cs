884a342d77475a8193eb60402718c17c16fb8f16
foziljonov7 <abdulvosidfoziljonov55@gmail.com>
2024-09-20 14:00:56 +0500

    Add Branch Services, DTOs

---
+++ Backend/Chayxana.BLL/DTOs/BranchDTOs/ModifyBranchDTO.cs
@@ -0,0 +1,13 @@
+using System.Text.Json.Serialization;
+
+namespace Chayxana.BLL.DTOs.BranchDTOs;
+
+public class ModifyBranchDTO
+{
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+    [JsonPropertyName("address")]
+    public string Address { get; set; }
+    [JsonPropertyName("phone_number")]
+    public string PhoneNumber { get; set; }
+}
---
+++ Backend/Chayxana.BLL/Interfaces/Branches/IBranchService.cs
@@ -0,0 +1,15 @@
+using Chayxana.BLL.DTOs.BranchDTOs;
+using Chayxana.BLL.DTOs.UserDTOs;
+
+namespace Chayxana.BLL.Interfaces.Branches;
+
+public interface IBranchService
+{
+    Task<IEnumerable<BranchDTO>> RetrieveAllBranchesAsync(CancellationToken cancellationToken = default);
+    Task<BranchDTO> RetrieveBranchAsync(long id, CancellationToken cancellationToken = default);
+    Task<BranchDTO> AddBranchAsync(AddBranchDTO newBranch, CancellationToken cancellationToken = default);
+    Task<bool> RemoveBranchAsync(long id, CancellationToken cancellationToken = default);
+    Task<BranchDTO> ModifyBranchAsync(long id, ModifyBranchDTO branch, CancellationToken cancellationToken = default);
+    Task<IEnumerable<EmployeeDTO>> RetrieveEmployeesByBranchIdAsync(long id, CancellationToken cancellationToken = default);
+    //Task<IEnumerable<RoomDTO>> RetrieveRoomsByBranchIdAsync(long id, CancellationToken cancellationToken = default);
+}
