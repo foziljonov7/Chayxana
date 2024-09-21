d67ff860e8e6b7047330b38a4331f0e4a1189e22
foziljonov7 <abdulvosidfoziljonov55@gmail.com>
2024-09-21 13:24:14 +0500

    ......

---
+++ Backend/Chayxana.BLL/DTOs/RoomDTOs/RoomDTO.cs
@@ -0,0 +1,21 @@
+﻿using Chayxana.Domain.Enums;
+using System.Text.Json.Serialization;
+
+namespace Chayxana.BLL.DTOs.RoomDTOs;
+
+public class RoomDTO
+{
+    [JsonPropertyName("id")]
+    public long Id { get; set; }
+    [JsonPropertyName("branch_id")]
+    public long BranchId { get; set; }
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+    [JsonPropertyName("description")]
+    public string Description { get; set; }
+    [JsonPropertyName("price")]
+    public double Price { get; set; }
+    //public ICollection<Booking> Bookings { get; set; }
+    [JsonPropertyName("status")]
+    public Status Status { get; set; }
+}
